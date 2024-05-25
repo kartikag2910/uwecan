a13f0d8aa3fa533813cd8458e03e1f016888e62c
kartikag2910 <kartikagarwal2910@gmail.com>
2024-05-25 23:26:17 +0530

    new change

--- Spring/Uwecan/src/main/java/com/example/Uwecan/Controllers/LoginController.java
+++ Spring/Uwecan/src/main/java/com/example/Uwecan/Controllers/LoginController.java
@@ -5,12 +5,10 @@ import com.example.Uwecan.Service.LoginService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class LoginController {
 @Autowired
     LoginService loginService;
--- Spring/Uwecan/src/main/java/com/example/Uwecan/UwecanApplication.java
+++ Spring/Uwecan/src/main/java/com/example/Uwecan/UwecanApplication.java
@@ -2,9 +2,6 @@ package com.example.Uwecan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UwecanApplication {
@@ -13,15 +10,6 @@ public class UwecanApplication {
 		SpringApplication.run(UwecanApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedMethods("*")
-						.allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
+
 
 }
