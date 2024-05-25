90b6b898099805558ca8439047763b50a43671a2
kartikag2910 <kartikagarwal2910@gmail.com>
2024-05-25 23:11:41 +0530

    deleted file.

--- Spring/Uwecan/src/main/java/com/example/Uwecan/UwecanApplication.java
+++ Spring/Uwecan/src/main/java/com/example/Uwecan/UwecanApplication.java
@@ -2,12 +2,26 @@ package com.example.Uwecan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UwecanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UwecanApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
