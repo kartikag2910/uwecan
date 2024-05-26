86b4dbc15e16d957e53c218fc98c1bf713769e3c
kartikag2910 <kartikagarwal2910@gmail.com>
2024-05-26 12:24:00 +0530

    Update LoginController.java

--- Spring/Uwecan/src/main/java/com/example/Uwecan/Controllers/LoginController.java
+++ Spring/Uwecan/src/main/java/com/example/Uwecan/Controllers/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/login-controller")
 public class LoginController {
 @Autowired
     LoginService loginService;
